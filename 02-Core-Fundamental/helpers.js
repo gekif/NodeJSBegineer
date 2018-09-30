3e863486546f7be4c3ffb4180db018e476d14a66
Gekif <dzulfikar.maulana@gmail.com>
2018-09-30 16:22:11 +0700

    02-CoreFundamentalV.4: objects

---
+++ 02-Core-Fundamental/helpers.js
@@ -0,0 +1,17 @@
+const object = require('./lib');
+
+/*
+module.exports.title = 'NODE';
+module.exports.title2 = 'NODE 2';
+module.exports.title3 = 'NODE 3';
+module.exports.title4 = () => {
+    console.log('Wohooo');
+};
+*/
+
+
+// console.log(module.exports);
+// console.log(module.exports.title4());
+// console.log(object.cal(5, 2));
+// console.log(`The sum is ${object.cal(5, 2)}`);
+console.log(`The value is ${object.value}`);
