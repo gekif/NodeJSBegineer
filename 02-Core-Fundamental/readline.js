e84187566b400dae557a70d3a7ac10a32fe6d498
Gekif <dzulfikar.maulana@gmail.com>
2018-09-30 16:03:01 +0700

    02-CoreFundamentalV.2: Readlines

---
+++ 02-Core-Fundamental/readline.js
@@ -0,0 +1,22 @@
+var readline = require('readline');
+var util = require('util');
+var RL = readline.createInterface(process.stdin, process.stdout);
+
+RL.question('What is your name? ', (name) => {
+    // console.log(name);
+    // RL.exit();
+
+    RL.setPrompt(`${name} how old are you? `);
+    RL.prompt();
+    RL.on('line', (age) => {
+        if (age < 18) {
+            util.log(`${name.trim()} karena kamu ${age} tahun, kamu gak bisa masuk`);
+            RL.close();
+        } else {
+            util.log(`${name.trim()} karena kamu ${age} tahun, kamu masuk`);
+            RL.close();
+        }
+    });
+
+});
+
