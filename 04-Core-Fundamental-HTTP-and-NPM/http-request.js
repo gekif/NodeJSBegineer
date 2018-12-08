43ed44734068a560d038a4ffa7df8f7d59a5bc66
Gekif <dzulfikar.maulana@gmail.com>
2018-12-08 19:28:05 +0700

    04-CoreFundamentalV.1: HTTP Native Requests

---
+++ 04-Core-Fundamental-HTTP-and-NPM/http-request.js
@@ -0,0 +1,24 @@
+const https = require('https');
+const fs = require('fs');
+
+const url = 'https://jsonplaceholder.typicode.com/posts';
+
+https.get(url, res => {
+    res.setEncoding('utf8');
+    let body = '';
+    res.on('data', data => {
+        body += data;
+    });
+
+    res.on('end', () => {
+        /*fs.writeFile('data.json', body, 'utf8', (err) => {
+            if (err) return err;
+            console.log('just pulled the post and created file');
+        });*/
+
+        body = JSON.parse(body);
+        console.log(`
+            ${body[0].title}
+        `);
+    });
+});
