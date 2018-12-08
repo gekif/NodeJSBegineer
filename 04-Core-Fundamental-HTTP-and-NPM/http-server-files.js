30d443ceb2fb628756a58ad9404a5d8ead098357
Gekif <dzulfikar.maulana@gmail.com>
2018-12-08 20:23:37 +0700

    04-CoreFundamentalV.3: Serving Files

---
+++ 04-Core-Fundamental-HTTP-and-NPM/http-server-files.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((req, res) => {
+    // console.log(req);
+    console.log(req.method);
+
+    if (req.url === '/') {
+        fs.readFile('./global.html', 'UTF-8', (err, data) => {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(data);
+        });
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('404 ERROR could not find your data');
+    }
+
+}).listen(3333);
+
+console.log('listening');
