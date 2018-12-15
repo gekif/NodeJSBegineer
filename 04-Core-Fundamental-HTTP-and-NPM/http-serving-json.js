82f0aa0a91c4a0c5c68e9a352c791a39bb1c337c
Gekif <dzulfikar.maulana@gmail.com>
2018-12-15 16:34:58 +0700

    04-CoreFundamentalV.4: HTTP and Json Data Part 1

---
+++ 04-Core-Fundamental-HTTP-and-NPM/http-serving-json.js
@@ -0,0 +1,25 @@
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
+
+const url = 'https://jsonplaceholder.typicode.com/posts';
+
+http.createServer((req, serverRes) => {
+    if (req.method === 'GET' && req.url === '/posts') {
+        https.get(url, (httpRes) => {
+            httpRes.on('data', data => {
+                httpRes.setEncoding('utf8');
+                console.log(data);
+            });
+
+        httpRes.ond('end', () => {
+            serverRes.end();
+            console.log('its over');
+        });
+
+        });
+    }
+
+}).listen(4444);
+
+console.log('Server is listening');
