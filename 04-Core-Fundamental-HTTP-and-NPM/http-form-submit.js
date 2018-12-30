dffb641d70b0cd0086d2b29cf324751256308975
Gekif <dzulfikar.maulana@gmail.com>
2018-12-30 12:14:16 +0700

    05-Express-FrameworkV.5: Submitting Express With Post Data

--- 04-Core-Fundamental-HTTP-and-NPM/http-form-submit.js
+++ 04-Core-Fundamental-HTTP-and-NPM/http-form-submit.js
@@ -7,7 +7,7 @@ http.createServer((req, res) => {
     if (req.method === 'GET') {
         res.writeHead(200, {'Content-Type': 'text/html'});
 
-        fs.readFile('./http-form.html', 'UTF-8', (err, data) => {
+        fs.readFile('./http-index.html', 'UTF-8', (err, data) => {
             if (err) throw err;
             res.write(data);
             res.end();
