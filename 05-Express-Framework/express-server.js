5f092ac262336a8dd501b1259f075e80663a69fd
Gekif <dzulfikar.maulana@gmail.com>
2018-12-30 11:01:17 +0700

    05-Express-FrameworkV.2: Routes

--- 05-Express-Framework/express-server.js
+++ 05-Express-Framework/express-server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 let app = express();
 
-app.listen(9999);
+const port = process.env.PORT || 9999;
+
+app.get('/', (req, res) => {
+    res.send('<h1>HELLO</h1>')
+});
+
+app.get('/api', (req, res) => {
+    // res.send('<h1>API PAGE</h1>');
+    res.json({
+        name: 'Fikar'
+    });
+});
+
+app.listen(port);
 
 console.log('it is working');
