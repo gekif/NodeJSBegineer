648a27202d931c73b45e300ff307bcfd4fee395b
Gekif <dzulfikar.maulana@gmail.com>
2018-12-30 11:38:59 +0700

    05-Express-FrameworkV.4: Middleware

---
+++ 05-Express-Framework/express-middleware.js
@@ -0,0 +1,35 @@
+const express = require('express');
+
+let app = express();
+
+app.use('/css', express.static(__dirname + '/public'));
+
+// app.use('/css', (req, res, next) => {
+// app.use('/', (req, res, next) => {
+app.use((req, res, next) => {
+    console.log('MIDDLEWARE');
+    next();
+});
+
+
+
+app.get('/', (req, res) => {
+    // res.send('HOME');
+    res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="css/style.css">
+        <title>Title</title>
+    </head>
+    <body>
+        <h1>Hello</h1>
+    </body>
+    </html>
+    `);
+});
+
+app.listen(9999);
+
+console.log('it is working');
