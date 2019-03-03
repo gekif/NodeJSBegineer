5812d2961b053fc0a723d0886e52f9320feaf015
Gekif <dzulfikar.maulana@gmail.com>
2019-03-03 13:50:32 +0700

    06-WebSocket V4: Client Side Data Handling

--- 06-WebSockets/websocket-client.js
+++ 06-WebSockets/websocket-client.js
@@ -4,6 +4,19 @@ WS.onmessage = (payload) => {
     console.log(payload.data);
 };
 
+WS.onopen = () => {
+    // console.log('CONNECTION OPEN');
+    displayTitle('CONNECTED TO SERVER');
+};
+
+WS.onclose = () => {
+    console.log('CONNECTION CLOSE');
+};
+
+let displayTitle = (title) => {
+    document.querySelector('h1').innerHTML = title;
+};
+
 document.forms[0].onsubmit = () => {
     let input = document.getElementById('message');
     WS.send(input.value);
