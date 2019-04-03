c74b4cf755f15a27012ba26d3c2f45f20e591118
Gekif <dzulfikar.maulana@gmail.com>
2019-04-03 20:00:09 +0700

    07-MongoDB: Connecting to MongoDB using an ODM - Mongoose

--- 07-MongoDB/app.js
+++ 07-MongoDB/app.js
@@ -1,12 +1,22 @@
-var MongoClient = require('mongodb').MongoClient
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/animals', {useMongoClient: true});
+mongoose.connection
+    .once('open', () => console.log('CONNECTED'))
+    .on('error', (err) => {
+        console.log('could bot connect', err);
+    });
+
+
+/*var MongoClient = require('mongodb').MongoClient
 
 MongoClient.connect('mongodb://localhost:27017/animals', function (err, db) {
     if (err) throw err
     console.log('CONNECTED');
 
-    /*db.collection('mammals').find().toArray(function (err, result) {
+    /!*db.collection('mammals').find().toArray(function (err, result) {
         if (err) throw err
 
         console.log(result)
-    })*/
-})
+    })*!/
+})*/
