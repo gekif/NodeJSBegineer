ab55482201e8200421d586ea9efede91d1ccdd73
Gekif <dzulfikar.maulana@gmail.com>
2019-04-06 22:10:40 +0700

    08-Mongoose: Creating Model Part 1

--- 08-Mongoose/app.js
+++ 08-Mongoose/app.js
@@ -1,6 +1,6 @@
-
 // Create Schema For The Database
 const mongoose = require('mongoose');
+const User = require('./models/User');
 
 mongoose.connect('mongodb://localhost:27017/mongoose', {useMongoClient: true});
 mongoose.connection
