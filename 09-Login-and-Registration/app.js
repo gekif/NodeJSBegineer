035ce0695438dee488c97329786a23745c31258d
Gekif <dzulfikar.maulana@gmail.com>
2019-04-14 20:49:10 +0700

    09-Login and Registration: Registration Part 2 - Adding Our Body Parser

--- 09-Login-and-Registration/app.js
+++ 09-Login-and-Registration/app.js
@@ -3,6 +3,15 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const User = require('./models/User');
+
+
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 
 
