8aa302a0a2ee90fbd1f3a3c44bf755f92422de38
Gekif <dzulfikar.maulana@gmail.com>
2019-04-14 16:46:30 +0700

    08-Mongoose: Fetching Data

--- 08-Mongoose/app.js
+++ 08-Mongoose/app.js
@@ -6,13 +6,16 @@ const app = express();
 const bodyParser = require('body-parser');
 
 
+
 // Body Parser Used
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
+
 // Add Promises
 mongoose.Promise = global.Promise;
+
 
 
 // Mongoose Connection
@@ -25,10 +28,12 @@ mongoose.connection
 );
 
 
+
 // Express Route
 app.get('/', (req, res) => {
     res.send("ROOT");
 });
+
 
 
 app.post('/users', (req, res) => {
@@ -57,8 +62,24 @@ app.post('/users', (req, res) => {
 
 
 
+// Fetch Data
+app.get('/users', (req, res) => {
+
+    // Find All Data
+    User.find({}).then(users => {
+        res.send(users);
+
+    // Catch Error
+    }).catch(err => {
+            res.status(200).send('USER CANT FIND BECAUSE ....');
+    });
 
 
+});
+
+
+
+// Port Connection
 const port = 4444 || process.env.PORT;
 
 app.listen(port, () => {
