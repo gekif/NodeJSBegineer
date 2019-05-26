2d99c22a4cbac00e71a235ab5fc9fe052ae583b5
Gekif <dzulfikar.maulana@gmail.com>
2019-05-26 11:30:27 +0700

    10-CMS Project Section: Loading and Using Routes

--- cms/app.js
+++ cms/app.js
@@ -1,17 +1,25 @@
+// Define the driver
 const express = require('express');
 const app = express();
 const path = require('path');
 const exphbs = require('express-handlebars');
 
+// Directory of public
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Set View Engine
 app.engine('handlebars', exphbs({defaultLayout: 'home'}));
 app.set('view engine', 'handlebars');
 
-const main = require('./routes/home/main');
+// Load Routes
+const home = require('./routes/home/index');
+const admin = require('./routes/admin/index');
 
-app.use('/', main);
+// Use Routes
+app.use('/', home);
+app.use('/admin', admin);
 
-app.listen(4500, () => {
-    console.log(`listening on port 4500`);
+// Port for website
+app.listen(80, () => {
+    console.log(`listening on port 80`);
 });
