02a3586810f729dda44589f2fe5447e8ddf685ca
Gekif <dzulfikar.maulana@gmail.com>
2019-09-02 22:28:51 +0700

    10-CMS Project Section: POSTS - Creating Post Routes Part 1

--- cms/app.js
+++ cms/app.js
@@ -14,12 +14,14 @@ app.set('view engine', 'handlebars');
 // Load Routes
 const home = require('./routes/home/index');
 const admin = require('./routes/admin/index');
+const posts = require('./routes/admin/posts');
 
 // Use Routes
 app.use('/', home);
 app.use('/admin', admin);
+app.use('/admin/posts', posts);
 
 // Port for website
-app.listen(80, () => {
-    console.log(`listening on port 80`);
+app.listen(4500, () => {
+    console.log(`listening on port 4500`);
 });
