b92e75707608ea21d6b9e5b818f3803f4b922ffc
Gekif <dzulfikar.maulana@gmail.com>
2019-09-04 16:59:25 +0700

    10-CMS Project Section: POSTS - Testing Our Post Model

--- cms/routes/admin/posts.js
+++ cms/routes/admin/posts.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../../models/Post');
 
 
 router.all('/*', (req, res, next) => {
@@ -19,11 +20,31 @@ router.get('/create', (req, res) => {
 
 /**
  * Connecting to the database
+ * Insert data to the database
  */
 router.post('/create', (req, res) => {
-    // res.send('WORKED');
 
-    console.log(req.body);
+    // Create variable to set up the initial allow comments
+    let allowComments = true;
+
+    (req.body.allowComments) ? allowComments = true : allowComments = false;
+/*
+    if (req.body.allowComments) {
+        allowComments = true;
+    } else {
+        allowComments = false;
+    }*/
+
+    Post({
+        title: req.body.title,
+        status: req.body.status,
+        allowComments: req.body.allowComments,
+        body: req.body.body
+    });
+
+    // console.log(req.body);
+
+    // console.log(req.body.allowComments);
 });
 
 
