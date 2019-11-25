f18aabf24c0c116fc5e98cb249dd2826af725570
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-25 15:05:34 +0700

    14 CMS Project - Something about SESSIONS  FLASH: 114 Installing Module

--- cms/app.js
+++ cms/app.js
@@ -7,39 +7,58 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const upload = require('express-fileupload');
+const session = require('express-session');
+const flash = require('connect-flash');
 
 
 // Fix Deprecated Promises
 mongoose.Promise = global.Promise;
+
 
 // Connect to MongoDB using Mongoose
 mongoose.connect('mongodb://localhost:27017/cms', {useMongoClient: true}).then(db => {
     console.log("MONGO connected")
 }).catch(error => console.log(error));
 
+
 // Directory of public
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Register handlebars helpers
 const {select} = require('./helpers/handlebars-helpers');
 
+
 // Set View Engine
 app.engine('handlebars', exphbs({defaultLayout: 'home', helpers: {select: select}}));
 app.set('view engine', 'handlebars');
+
 
 // Upload Middleware
 app.use(upload({
     limits: { fileSize: 50 * 1024 * 1024 }
 }));
 
-
-
 // Set Up Body Parser
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+
 // Method Override
 app.use(methodOverride('_method'));
+
+
+// Session
+app.use(session({
+    secret: 'gekif',
+    resave: true,
+    saveUninitialized: true,
+
+}));
+
+app.use(flash);
+
+
 
 // Load Routes
 const home = require('./routes/home/index');
@@ -50,6 +69,7 @@ const posts = require('./routes/admin/posts');
 app.use('/', home);
 app.use('/admin', admin);
 app.use('/admin/posts', posts);
+
 
 // Port for website
 app.listen(4500, () => {
