ea65e7747cb10e2e259f9b023ef487d7b1db3d2b
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-26 16:56:11 +0700

    18 CMS Project - Authentication - Registration Section : 142 Adding our User to the Post route

--- cms/routes/home/index.js
+++ cms/routes/home/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../../models/Post');
 const Category = require('../../models/Category');
+const User = require('../../models/User');
 
 
 router.all('/*', (req, res, next) => {
@@ -38,6 +39,21 @@ router.get('/login', (req, res) => {
 
 router.get('/register', (req, res) => {
     res.render('home/register');
+});
+
+
+router.post('/register', (req, res) => {
+
+    const newUser = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+
+
+    res.send('home/register');
 });
 
 
