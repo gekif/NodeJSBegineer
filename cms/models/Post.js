88740de60c307d7617f44dcefab9c58d4ad68e7c
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-26 14:54:35 +0700

    17 CMS Project - Categories : 137 Adding a Select to the Edit Post View

--- cms/models/Post.js
+++ cms/models/Post.js
@@ -4,6 +4,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const PostSchema = new Schema({
 
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: 'categories'
+    },
+
     title: {
         type: String,
         required: true
@@ -32,6 +37,8 @@ const PostSchema = new Schema({
         type: Date,
         default: Date.now()
     }
+
+
 
 });
 
