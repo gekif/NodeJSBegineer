797e99153d429195c70a69860b77a4ce2c98fc81
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-12-02 14:43:04 +0700

    24 CMS Project Extra Features - Pagination: 191 Creating dynamic list items in Handlebars function part 3

--- cms/helpers/handlebars-helpers.js
+++ cms/helpers/handlebars-helpers.js
@@ -34,7 +34,18 @@ module.exports = {
         for (; i <= (Number(options.hash.current) + 4) && i <= options.hash.pages; i++) {
             if (i === options.hash.current) {
                 output += `<li class="page-item active"><a class="page-link">${i}</a></li>`;
+            } else {
+                output += `<li class="page-item"><a href="?page=${i}" class="page-link">${i}</a></li>`;
             }
+
+            // Maybe the dots
+        }
+
+        if (options.hash.current === options.hash.pages) {
+
+            output += `<li class="page-item disabled"><a class="page-link">Last</a></li>`;
+        } else {
+            output += `<li class="page-item"><a href="?page=${options.hash.pages}" class="page-link">Last</a></li>`;
         }
 
         return output;
